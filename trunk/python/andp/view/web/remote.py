494264c890be7d965cb9248c722a8a7fd87f3e39
audunv <audunv@d08e4f62-0285-11de-99c0-75d572ba1ddd>
2009-05-24 13:10:26 +0000

    Works, but doesn't look very good

--- trunk/python/andp/view/web/remote.py
+++ trunk/python/andp/view/web/remote.py
@@ -25,7 +25,9 @@ This module handles the remote control interface
 
 """
 
-import subprocess
+import subprocess, cStringIO
+
+from PIL import Image, ImageDraw, ImageFont
 
 from mod_python import apache
 
@@ -43,6 +45,7 @@ class RemoteQR(Page):
     
     path = "/remote/qr"
     showDebug = False
+    fontPath = "/usr/share/fonts/truetype/ttf-bitstream-vera/Vera.ttf" # Quite randomly chosen
 
     def Main(self):
         host = self.req.get_remote_host(apache.REMOTE_NOLOOKUP)
@@ -54,10 +57,35 @@ class RemoteQR(Page):
         proc = subprocess.Popen(cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 
         uri = "http://%s.%s/%s" % (self.req.config["network"]["host"], self.req.config["network"]["domain"], sessionID)
-        img = proc.communicate(uri)[0]
+        buf = cStringIO.StringIO(proc.communicate(uri)[0])
+        qrImage = Image.open(buf)
+
+        font = ImageFont.truetype(self.fontPath, 256) # Big enough to scale image down well
+
+        topText = "%s.%s/" % (self.req.config["network"]["host"], self.req.config["network"]["domain"])
+        topImage = Image.new("RGB", font.getsize(topText), (0, 0, 0))
+        topDraw = ImageDraw.Draw(topImage)
+        topDraw.text((0, 0), topText, font = font)
+        del topDraw
+        topImage = topImage.resize((qrImage.size[0], (float(qrImage.size[0]) / topImage.size[0]) * topImage.size[1]), Image.ANTIALIAS)
+
+        bottomText = "%s" % (sessionID,)
+        bottomImage = Image.new("RGB", font.getsize(bottomText), (0, 0, 0))
+        bottomDraw = ImageDraw.Draw(bottomImage)
+        bottomDraw.text((0, 0), bottomText, font = font)
+        del bottomDraw
+        bottomImage = bottomImage.resize((qrImage.size[0], (float(qrImage.size[0]) / bottomImage.size[0]) * bottomImage.size[1]), Image.ANTIALIAS)
+
+        compositeImage = Image.new("RGB", (qrImage.size[0], topImage.size[1] + qrImage.size[1] + bottomImage.size[1]), (0, 0, 0))
+        compositeImage.paste(topImage, (0, 0))
+        compositeImage.paste(qrImage, (0, topImage.size[1]))
+        compositeImage.paste(bottomImage, (0, topImage.size[1] + qrImage.size[1]))
+
+        buf = cStringIO.StringIO()
+        compositeImage.save(buf, "PNG")
         
         self.SendHeader(contentType = "image/png")
-        self.Write(img)
+        self.Write(buf.getvalue())
         
         return apache.OK
 
